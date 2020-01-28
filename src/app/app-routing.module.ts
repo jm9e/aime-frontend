bfe9c71d94dfa792ddbceeacbc1434b4788ae300
Julian Matschinske <julian.matschinske@wzw.tum.de>
2020-01-28 13:18:25 +0100

    Add index and questionnaire pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionnaireComponent } from './pages/questionnaire/questionnaire.component';
+import { IndexComponent } from './pages/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+  },
+  {
+    path: 'questionnaire',
+    component: QuestionnaireComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
