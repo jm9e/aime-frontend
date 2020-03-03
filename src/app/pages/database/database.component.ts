ec87bb0cd7938f763d6191684da7e03a727811f8
Julian Matschinske <julian.matschinske@wzw.tum.de>
2020-03-03 11:29:13 +0100

    Set page limit to 3 for testing

--- src/app/pages/database/database.component.ts
+++ src/app/pages/database/database.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {createQuestionnaire, IReport} from '../../interfaces';
-import {BehaviorSubject} from 'rxjs';
-import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IReport } from '../../interfaces';
+import { BehaviorSubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-database',
@@ -21,10 +21,10 @@ export class DatabaseComponent implements OnInit {
   public searchMethod = true;
   public searchReproducibility = true;
   public reportCited: IReport | null = null;
-  public expanded: {[key: string]: any} = {};
+  public expanded: { [key: string]: any } = {};
   public currentPage = 0;
   public pages: number[] = [];
-  private ITEMS_PER_PAGE = 5;
+  private ITEMS_PER_PAGE = 3;
 
   constructor(private http: HttpClient) {
     this.query.pipe(
@@ -54,20 +54,22 @@ export class DatabaseComponent implements OnInit {
   }
 
   public search(offset = 0) {
-    this.http.get<any>(`/api/search?q=${escape(this.query.getValue())}&f=${this.getFields()}&o=${offset}`).subscribe((resp) => {
-      this.resultsCount = resp.count;
-      this.results = resp.results.map((result) => {
-        result.date = new Date(result.date);
-        result.versions = result.versions.map((revision) => {
-          revision.date = new Date(revision.date);
-          return revision;
+    this.http.get<any>(
+      `/api/search?q=${escape(this.query.getValue())}&f=${this.getFields()}&o=${offset}&l=${this.ITEMS_PER_PAGE}`)
+      .subscribe((resp) => {
+        this.resultsCount = resp.count;
+        this.results = resp.results.map((result) => {
+          result.date = new Date(result.date);
+          result.versions = result.versions.map((revision) => {
+            revision.date = new Date(revision.date);
+            return revision;
+          });
+          return result;
         });
-        return result;
+        this.lastQuery = resp.query;
+        this.currentPage = 0;
+        this.pages = Array(Math.ceil(this.resultsCount / this.ITEMS_PER_PAGE)).fill(0).map((x, i) => i);
       });
-      this.lastQuery = resp.query;
-      this.currentPage = 0;
-      this.pages = Array(Math.ceil(this.resultsCount / this.ITEMS_PER_PAGE)).fill(0).map((x, i) => i);
-    });
   }
 
   public setPage(i) {
