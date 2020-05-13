a853d75713d7b732d02d7e44383b3ad37b6f59cd
Julian O. Matschinske <julian@vyze.ai>
2020-05-13 18:09:55 +0200

    Add backend call

--- src/app/pages/survey/survey.component.ts
+++ src/app/pages/survey/survey.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-survey',
@@ -8,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class SurveyComponent implements OnInit {
 
   public suggestedQuestion = '';
+  public questionReason = '';
+  public generalFeedback = '';
+  public contactDetails = '';
+  public requestAuthorship = true;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
+  }
+
+  public async submitSurvey() {
+    const reqObj = {
+      suggestedQuestion: this.suggestedQuestion,
+      questionReason: this.questionReason,
+      generalFeedback: this.generalFeedback,
+      contactDetails: this.contactDetails,
+      requestAuthorship: this.requestAuthorship,
+    };
+    this.suggestedQuestion = '';
+    this.questionReason = '';
+    this.generalFeedback = '';
+    this.contactDetails = '';
+    await this.http.post(``, reqObj);
   }
 
 }
